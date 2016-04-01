602db514603fd2318c586565220df8449c8de113
Christine Oo <christine@favoritemedium.com>
2016-04-01 11:19:08 +0900

    temporary remove style.css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Button from 'react-toolbox/lib/button';
-import style from '../style/style.scss'
 
 export default class App extends Component {
   render() {
