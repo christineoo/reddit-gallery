c22320ef8ddbb3e6df02a4df2283c911f76d1688
Christine Oo <christine@favoritemedium.com>
2016-04-14 12:30:56 +0900

    [UI] Update header title

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ export default class Header extends Component {
   render() {
     return (
       <AppBar fixed flat>
-        <a href="/home">Reddit Gallery</a>
+        <a href="/home">Reddit Feeds</a>
       </AppBar>
     )
   }
