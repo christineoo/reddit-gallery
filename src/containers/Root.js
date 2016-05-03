d409338c5c30fc66f15e279a4ba7442c589118df
Christine Oo <christine@favoritemedium.com>
2016-05-03 16:29:44 +0900

    [CLEAN UP]

--- src/containers/Root.js
+++ src/containers/Root.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react'
 import { Provider } from 'react-redux'
 import configureStore from '../store/configureStore'
-import AsyncApp from './AsyncApp'
 import ToolboxApp from 'react-toolbox/lib/app'
-import Header from '../components/header';
+import App from '../App';
 
 const store = configureStore()
 
@@ -12,8 +11,7 @@ export default class Root extends Component {
     return (
       <Provider store={store}>
         <ToolboxApp>
-          <Header />
-          <AsyncApp />
+          <App />
         </ToolboxApp>
       </Provider>
     )
